8d0293faf127b70f848d55834cc5900d124108ed
Olaoluwayemi Rasheed <rasholayemi@gmail.com>
2022-05-04 23:08:48 +0100

    push to heroku

--- notepad/settings.py
+++ notepad/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 from datetime import timedelta
@@ -154,3 +154,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
